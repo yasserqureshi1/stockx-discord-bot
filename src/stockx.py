28256a2ebfa47370dfac486e6f944c1642cb2647
Yasser Qureshi <49324530+yasserqureshi1@users.noreply.github.com>
2023-04-09 01:38:28 +0100

    Update stockx.py

--- src/stockx.py
+++ src/stockx.py
@@ -9,17 +9,15 @@ def search(query):
         'accept': 'application/json',
         'accept-encoding': 'utf-8',
         'accept-language': 'en-GB,en;q=0.9',
-        'app-platform': 'Iron',
-        'referer': 'https://stockx.com/en-gb',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.62 Safari/537.36',
-        'x-requested-with': 'XMLHttpRequest'
-    }
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'x-requested-with': 'XMLHttpRequest',
+        'app-platform': 'Iron',
+        'app-version': '2022.05.08.04',
+        'referer': 'https://stockx.com/'
+        }
 
     html = requests.get(url=url, headers=headers)
     output = json.loads(html.text)
